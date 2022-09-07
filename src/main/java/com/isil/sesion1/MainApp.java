4d525769f3c72d8c554c2785cce36fb40091cd60
Edu Zuñiga Santillan <edu@MacBook-Pro.local>
2022-09-07 10:44:41 -0500

    Se agrega prepared statement

--- src/main/java/com/isil/sesion1/MainApp.java
+++ src/main/java/com/isil/sesion1/MainApp.java
@@ -1,30 +1,45 @@
 package com.isil.sesion1;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public class MainApp {
 
     public static void main(String[] args) throws Exception {
 
+        // 1. Cargar driver
         Class.forName("com.mysql.cj.jdbc.Driver");
-
+        // 2. Crear conexion
         Connection con = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/TEST",
+                "jdbc:mysql://localhost:3306/DESAI",
                 "root",
                 "budakar01"
         );
 
-        Statement stmt= con.createStatement();
+        // 3. Crear statement
+        // 3.1 Statement mantenimiento
+        Statement stCreate = con.createStatement();
+        int filasAfectadas=
+                stCreate.executeUpdate("UPDATE USERS SET NAME='JUAN' WHERE IDUSER=1");
+        System.out.println("Filas afectadas: " + filasAfectadas);
 
-        ResultSet result = stmt.executeQuery("select * from Users");
+        // 3.2 Statement consulta
+        PreparedStatement preparedStatement =
+                con.prepareStatement("select * from Users where idUser=?");
 
-        while(result.next()){
-            System.out.println(result.getString(2));
+        preparedStatement.setInt(1, 1);
+
+        // 4. Ejecutar query
+        ResultSet resultSet2 = preparedStatement.executeQuery();
+
+
+        // 5. Recorrer resultados
+        while(resultSet2.next()){
+            System.out.println(resultSet2.getString("name") + "\n"+
+                    resultSet2.getString("phone") + "\n"+
+                    resultSet2.getString("city"));
         }
-
+        // 6. Cerrar conexion
+        con.close();
     }
 
 }
